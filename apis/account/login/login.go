aa3f5b6837f32a495de3fa68b5d1e75254390c31
Yunseo Hwang <yunseo.h68@gmail.com>
2019-12-29 12:58:25 +0900

    Fix : http 상태 코드를 하드코딩하던 방식에서  http패키지에서 제공하는 상수 사용으로 수정

--- apis/account/login/login.go
+++ apis/account/login/login.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"golang.org/x/crypto/bcrypt"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/mholt/binding"
@@ -42,15 +41,6 @@ func (res Response) Data() ([]byte, error) {
 	return json.Marshal(res)
 }
 
-// ComparePw 함수는 hash화된 Pw와 평문 Pw를 비교하는 함수입니다
-func ComparePw(hash, pw string) (bool, error) {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
-	if err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
 // URI 메서드는 Login API의 URI를 반환합니다
 func (api *API) URI() string {
 	return "/api/account/login"
@@ -62,29 +52,24 @@ func (api *API) Post(w http.ResponseWriter, req *http.Request, ps httprouter.Par
 
 	if errs := binding.Bind(req, ul); errs != nil {
 		log.Println(errs)
-		return &Response{200, "", false, "요청 메시지 파싱에 실패하였습니다"}
+		return &Response{http.StatusInternalServerError, "", false, "요청 메시지 파싱에 실패하였습니다"}
 	}
-	log.Println("Request UID : ", ul.UID)
-	log.Println("Request PW : ", ul.Pw)
 
 	u := new(user.User)
 	err := db.MongoDB.DB("gwahangmi").C("users").FindOne(context.TODO(), bson.M{"uid": ul.UID}).Decode(&u)
 
-	log.Println("DB UID : ", u.UID)
-	log.Println("DB PW : ", u.Pw)
-
 	if err != nil {
 		log.Println(err)
-		return &Response{404, "", false, "존재하지 않는 User입니다"}
+		return &Response{http.StatusNotFound, "", false, "존재하지 않는 User입니다"}
 	}
 
 	if u.UID == ul.UID {
-		pwOK, err := ComparePw(u.Pw, ul.Pw)
+		pwOK, err := user.ComparePw(u.Pw, ul.Pw)
 		if pwOK {
-			return &Response{200, u.Name, true, "로그인에 성공하셨습니다"}
+			return &Response{http.StatusOK, u.Name, true, "로그인에 성공하셨습니다"}
 		}
 		log.Println(err)
-		return &Response{200, "", false, "잘못된 PW입니다"}
+		return &Response{http.StatusOK, "", false, "잘못된 PW입니다"}
 	}
-	return &Response{200, "", false, "잘못된 ID입니다"}
+	return &Response{http.StatusOK, "", false, "잘못된 ID입니다"}
 }
